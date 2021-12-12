88d4540b33dd19a02576dafc52f4da6c962b8727
Aleh Varabei <vlsynme@gmail.com>
2021-12-12 11:58:56 +0300

    add new version

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Header from "../Header/Header";
 import RandomPlanet from "../RandomPlanet/RandomPlanet";
 import ItemList from "../ItemList/ItemList"
 import PlanetDetails from "../PlanetDetails/PlanetDetails"
 function App () {
+		let [planetId, setPlanetId] = useState("11");
+
     return (
         <div className="app">
             <div className="wrapper">
                 <Header/>
                 <RandomPlanet/>
                 <div className = "item-list-and-planet-details">
-                    <ItemList/>
-                    <PlanetDetails/>
+                    <ItemList onClick={(value: any) => setPlanetId(value)}/>
+                    <PlanetDetails id={planetId}/>
                 </div>
             </div>
         </div>
--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import './ItemList.css';
+import { getId } from '../PlanetDetails/PlanetDetails';
 
 import 'antd/dist/antd.css';
-import { Menu } from 'antd';
+import { Menu, Spin } from 'antd';
 
-function ItemList () {
-		let [planets, setPlanets] = useState([]);
-
+function ItemList (props: any) {
+		let [planets, setPlanets] = useState <any>([]);
+		let [loading, setLoading] = useState(true);
 		// сработает только после мотнирования компонента, при обновлении не будет срабатывать т.к в зависимостях указаны []
 		useEffect(() => {
 			getPlanets();
 		}, []);
 
 		async function getPlanets() {
+			setLoading(true);
 			let response = await fetch('https://swapi.dev/api/planets/?page=2');
 
 			let commits = await response.json();
 
 			setPlanets(commits.results);
+			setLoading(false);
 		}
 
 		let getMenuItems = () => {
 			return planets.map((item: any) => {
-				return <Menu.Item key={item.url}>{item.name}</Menu.Item>
+				return <Menu.Item key={getId(item.url)}>{item.name}</Menu.Item>
 			});
 		};
 
     return (
         <div className="item-list">
-            <Menu className="item-list-menu" mode="inline">
+						{loading && <Spin />}
+            {!loading && 
+						<Menu 
+							defaultSelectedKeys={["11"]} 
+							className="item-list-menu" 
+							mode="inline" 
+							onClick={(value) => props.onClick(value.key)}
+						>
 								{/* <Menu.Item key="1">Alderaan</Menu.Item>
                 <Menu.Item key="2">Yavin IV</Menu.Item>
                 <Menu.Item key="3">Hoth</Menu.Item>
@@ -37,7 +47,7 @@ function ItemList () {
                 <Menu.Item key="5">Bespin</Menu.Item>*/}
 
 							{getMenuItems()}
-            </Menu>
+            </Menu>}
         </div>
     );
 }
